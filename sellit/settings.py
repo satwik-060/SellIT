6fa30cd32dbbffef3bf89c3b7e21162ef1914ba2
satwik-060 <perisetla.srisatwik00@gmail.com>
2023-12-22 22:35:13 +0530

    deploying on render

--- sellit/settings.py
+++ sellit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j4ippt+3h39u4ontllpc8a(4h&^god(7aicz#@q^sl_(w)2otp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
